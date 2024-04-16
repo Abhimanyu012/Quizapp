64e17bd3116d88d2a3b05d164934380994ca0873
Abhimanyu Kumar <abhimanyucode2021@gmail.com>
2024-04-17 00:51:29 +0530

    bgc done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,21 +23,21 @@ const Home = () => {
 
     return (
         <>
-            <div className="d-flex justify-content-center align-items-center">
+            <div className="d-flex justify-content-center align-items-center ">
                 <HashLoader
                     color={'#3585c1'}
                     loading={loading}
                     size={100}
                     aria-label="Loading Spinner"
                     data-testid="loader"
-                    style={{ backgroundColor: '#4d4d4dcc', width: '100%', height: '100vh', position: 'absolute', top: '13%' }}
+                    style={{ backgroundColor: 'yellow', width: '100%', height: '100vh', position: 'absolute', top: '13%' }}
                 />
             </div>
             {
                 (url === '' || questions.length === 0)
                     ?
                     <div className="container my-3">
-                        <Text mb={'4'} fontSize='4xl'>Start your Quiz Now</Text>
+                        <Text mb={'4'} fontSize='5xl'>Start your Quiz Now</Text>
                         <hr />
                         <Form handleSubmit={handleSubmit} onChange={onChange} />
                     </div>
